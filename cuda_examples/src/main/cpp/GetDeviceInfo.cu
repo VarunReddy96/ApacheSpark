1ec3a6849ad154b1d24d1e856b03791534a57968
Peizhao Hu <ph@cs.rit.edu>
2019-10-20 20:09:19 -0400

    add get device info

---
+++ cuda_examples/src/main/cpp/GetDeviceInfo.cu
@@ -0,0 +1,26 @@
+// For infor about these example, see the links below
+// https://devblogs.nvidia.com/how-query-device-properties-and-handle-errors-cuda-cc/
+// https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__DEVICE.html#group__CUDART__DEVICE_1g1bf9d625a931d657e08db2b4391170f0
+#include <iostream>
+
+// Host code
+int main(void)
+{
+    int nDevices;
+
+      cudaGetDeviceCount(&nDevices);
+      for (int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Max Threads Per Block: %d\n", prop.maxThreadsPerBlock);
+        printf("  Max Threads Dim: x:%d, y:%d, z:%d\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+        printf("  Max Grid Size: x:%d, y:%d, z:%d\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+        printf("  Clock Rate (KHz): %d\n", prop.clockRate);
+        printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %f\n\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+      }
+
+}
